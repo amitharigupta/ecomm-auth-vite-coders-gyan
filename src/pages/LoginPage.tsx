5bf4451f4be6c1af7daa360c1e2cf420f52bdde9
Amit Gupta <amitg@VF096.local>
2024-04-27 15:31:53 +0530

    add tanstack query

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,9 +2,21 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { useRef } from 'react'
 import { Link } from 'react-router-dom'
 
 const LoginPage = () => {
+
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const loginHandler = () => {
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    console.log(email, password);
+    // make server call.
+  }
+
   return (
     <section className='flex justify-center items-center h-screen'>
       <Card className="w-full max-w-sm">
@@ -17,16 +29,16 @@ const LoginPage = () => {
         <CardContent className="grid gap-4">
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" placeholder="m@example.com" required />
+            <Input ref={emailRef} id="email" type="email" placeholder="m@example.com" required />
           </div>
           <div className="grid gap-2">
             <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
+            <Input ref={passwordRef} id="password" type="password" required />
           </div>
         </CardContent>
         <CardFooter>
           <div className='w-full'>
-            <Button className="w-full">Login</Button>
+            <Button className="w-full" onClick={loginHandler}>Login</Button>
             <div className="mt-4 text-center text-sm">
               Don't have an account?{" "}
               <Link to={"/auth/register"} className="underline">
